4d4eb6a13af4c7d17d60000d26720ec86357a5d0
ming.lau <ming.lau@candrholdings.com>
2020-03-13 19:00:54 +0800

    update list-images-name.py

--- list-images-name.py
+++ list-images-name.py
@@ -2,27 +2,38 @@ import os
 import sys
 import calendar
 import time
+from functools import reduce
+
 ts = calendar.timegm(time.gmtime())
 
+def createPath(x, y):
+    return os.path.join(x, y)
 
-mypath = 'raw-images\persian-cat'
+
+mypath = reduce(createPath, ['raw-images', 'persian-cat'])
 
 cwd = os.getcwd()
-print(ts)
 
 list = ""
+
 for index, fullFileName in enumerate(os.listdir(mypath), start=0):
     filename, file_extension = os.path.splitext(fullFileName)
     if filename and file_extension[1:] == 'jpg':
-        a = cwd + "\\" + mypath + "\\" + filename + file_extension
-        b = cwd + "\\" + mypath + "\\" + \
-            str(index) + '_'+str(ts) + file_extension
+        print(filename)
+        print(file_extension)
+        origin = reduce(createPath, [cwd, mypath, filename+file_extension])
+        new = reduce(createPath, [cwd, mypath, str(
+            index) + '_'+str(ts) + file_extension])
+        print(new)
 
         try:
-            os.rename(a, b)
+            os.rename(origin, new)
+
             list = list + str(index) + '_'+str(ts) + file_extension + "\n"
             print('done')
         except:
             print("An exception occurred")
 
-print(list)
+a = open('somefile.txt', 'w')
+a.write(list)
+a.close()
